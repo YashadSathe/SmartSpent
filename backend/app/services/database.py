554c2137fe3c38a2887688799570f612d9424201
CobraOmega <yashad04@gmail.com>
2025-11-10 05:50:59 +0530

    Site working with backend and frontend integration

--- backend/app/services/database.py
+++ backend/app/services/database.py
@@ -10,7 +10,7 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./expense_tracker.db"
 # Create Engine
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same-thread": False}
+    connect_args={"check_same_thread": False}
 )
 
 # Local Sessiom
